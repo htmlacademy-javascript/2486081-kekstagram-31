c36d646a5722c0c0cd80dd80bad625bed5252a8f
Roman <morozov9786@gmail.com>
2024-03-18 17:21:44 +0300

    Добавит в ивенты

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-import {getArrayPhotos} from './data.js';
-import {renderPhotos} from './miniature.js';
-renderPhotos(getArrayPhotos());
+import {getPhotos} from './data.js';
+import {renderPhotos, renderBigPhoto, renderComments} from './render-content.js';
+import {openBigPicture,closeBigPicture} from './event.js';
+
+renderPhotos(getPhotos());
+
+const pictures = document.querySelectorAll('.picture');
+const buttonClose = document.querySelector('.big-picture__cancel');
+
+for (const picture of pictures) {
+  picture.addEventListener('click', () => {
+    openBigPicture();
+    renderBigPhoto(picture);
+    renderComments(getPhotos());//Работает не правильно
+  });
+}
+
+buttonClose.addEventListener('click', () => {
+  closeBigPicture();
+  document.querySelector('.social__comments').innerHTML = '';
+});
