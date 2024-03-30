884c01379cbff2fe7f51f3246a130f901a9b07f7
Roman <morozov9786@gmail.com>
2024-03-31 02:45:16 +0300

    Сдлеает комменты по 5

--- js/render-content.js
+++ js/render-content.js
@@ -1,7 +1,7 @@
 import {openBigPicture,closeBigPicture} from './event.js';
 const templateComment = document.querySelector('.social__comment');
 const listComment = document.querySelector('.social__comments');
-
+const buttonAddComment = document.querySelector('.social__comments-loader');
 const template = document.querySelector('#picture').content.querySelector('a');
 const fragmentPicture = document.createDocumentFragment();
 const listPictures = document.querySelector('.pictures');
@@ -21,16 +21,40 @@ function renderPictures(picture) {
       document.querySelector('.big-picture__img').querySelector('img').src = url;
       document.querySelector('.social__caption').textContent = description;
       document.querySelector('.likes-count').textContent = likes;
-      document.querySelector('.social__comment-shown-count').textContent = 1;
-      document.querySelector('.social__comment-total-count').textContent = comments.length;
+      let number = 5;
+      const STEP = 5;
+      if (comments.length === 0) {
+        document.querySelector('.social__comment-shown-count').textContent = 0;
+        document.querySelector('.social__comment-total-count').textContent = 0;
+        buttonAddComment.classList.add('hidden');
+      }
 
-      listComment.innerHTML = '';
-      comments.forEach(({avatar, name, message}) => {
-        const commentElement = templateComment.cloneNode(true);
-        commentElement.querySelector('img').src = avatar;
-        commentElement.querySelector('img').alt = name;
-        commentElement.querySelector('.social__text').textContent = message;
-        listComment.append(commentElement);
+      addComments(comments);
+
+      function addComments(comment) {
+        const arrComment = comment;
+        const arrSliceComments = arrComment.slice(0 , number);
+        listComment.innerHTML = '';
+        arrSliceComments.forEach(({avatar, name, message}) => {
+          const commentElement = templateComment.cloneNode(true);
+          commentElement.querySelector('img').src = avatar;
+          commentElement.querySelector('img').alt = name;
+          commentElement.querySelector('.social__text').textContent = message;
+          listComment.append(commentElement);
+          document.querySelector('.social__comment-shown-count').textContent = arrSliceComments.length;
+          document.querySelector('.social__comment-total-count').textContent = arrComment.length;
+
+          if (arrSliceComments.length >= arrComment.length) {
+            buttonAddComment.classList.add('hidden');
+          } else {
+            buttonAddComment.classList.remove('hidden');
+          }
+        });
+      }
+      buttonAddComment.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        number += STEP;
+        addComments(comments);
       });
     });
   });
