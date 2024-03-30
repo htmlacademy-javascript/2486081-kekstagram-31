8105a763494cf09013788b0b41ae93fb8341f470
Roman <morozov9786@gmail.com>
2024-03-31 02:52:25 +0300

    main js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
 import {getPictures} from './data.js';
 import {renderPictures} from './render-content.js';
+import {openUploadedPicture, closeUploadedPicture} from './uploaded-model.js';
+
+const formElement = document.querySelector('.img-filters__form');
+formElement.action = 'https://31.javascript.htmlacademy.pro/kekstagram';
+formElement.method = 'POST';
+formElement.enctype = 'multipart/form-data';
 
 renderPictures(getPictures());
+
+const cancelUploadedPicture = document.querySelector('.img-upload__cancel');
+const uploadInput = document.querySelector('.img-upload__input');
+
+uploadInput.addEventListener('change', () => {
+  openUploadedPicture();
+
+});
+
+cancelUploadedPicture.addEventListener('click', () => {
+  closeUploadedPicture();
+
+});
+
